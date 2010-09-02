2a7aa564501e74c9993a6dc42875473738dcd370
Moya Rada <myb@mikebevz.com>
2010-09-02 15:39:45 +0200

    #16 Initial implementation of SoapServer

---
+++ test/SoapServerTest.php
@@ -0,0 +1,81 @@
+<?php
+use dk\nordsign\application\services;
+set_include_path(
+    get_include_path() . PATH_SEPARATOR . realpath("../src"));
+use com\mikebevz\xsd2php;
+
+require_once 'PHPUnit/Framework.php';
+require_once "com/mikebevz/xsd2php/SoapServer.php";
+//require_once "com/mikebevz/xsd2php/Php2Xml.php";
+
+class BindTest extends PHPUnit_Framework_TestCase
+{
+/**
+     * 
+     * @var xsd2php\SoapServer
+     */
+    private $tclass;  
+    
+    private $wsdl;
+    
+    private $options;
+    
+    private $expDir = "data/expected/SoapServer";
+    private $genDir = "data/generated/SoapServer";
+    
+    
+    protected function setUp ()
+    {
+        $this->options = array(
+            'soap_version' => SOAP_1_2,
+            'cache_wsdl' => WSDL_CACHE_NONE,
+            'features' => SOAP_SINGLE_ELEMENT_ARRAYS
+        );
+        
+        $this->wsdl = $this->expDir.DIRECTORY_SEPARATOR."NavService.wsdl";
+        $this->tclass = new xsd2php\SoapServer($this->wsdl,  $this->options);
+        
+        
+    }
+    protected function tearDown ()
+    {
+        $this->tclass = null;        
+    }
+    
+    public function test1() {
+        
+        require_once $this->expDir.DIRECTORY_SEPARATOR."services".DIRECTORY_SEPARATOR."NavService.php";
+        
+        $service = new services\NavService();
+        $this->tclass->setObject($service);
+                
+        $req = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body>
+        <ns4:updateContactCompany xmlns:ns2="urn:dk:nordsign:schema:ContactCompany" 
+        xmlns:ns3="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" 
+        xmlns:ns4="urn:dk:nordsign:application:services" 
+        xmlns:ns5="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2" 
+        xmlns:ns6="urn:dk:nordsign:schema:ContactPerson">
+        <ns2:ContactCompany>
+        <ns2:ExtID>DK234234234234234</ns2:ExtID><ns5:CompanyID>DK234234234234234</ns5:CompanyID><ns5:Name>TON s.r.o.</ns5:Name><ns5:Telephone>24234234222</ns5:Telephone><ns5:Telefax>12341234622</ns5:Telefax><ns3:Party><ns5:WebsiteURI>test.com</ns5:WebsiteURI></ns3:Party><ns2:BillingAddress><ns2:Address><ns5:Line>Nytorv 50</ns5:Line></ns2:Address><ns2:PostalCode>9000</ns2:PostalCode><ns2:City>Aalborg</ns2:City><ns2:State>Nordjylland</ns2:State><ns2:Country>DK</ns2:Country></ns2:BillingAddress>
+        </ns2:ContactCompany>
+        </ns4:updateContactCompany>
+        </soap:Body></soap:Envelope>';
+        
+        $req = '<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body>
+        <ns4:updateContactPerson xmlns:ns2="urn:dk:nordsign:schema:ContactCompany" 
+        xmlns:ns3="urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2" 
+        xmlns:ns4="urn:dk:nordsign:application:services" 
+        xmlns:ns5="urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2" 
+        xmlns:ns6="urn:dk:nordsign:schema:ContactPerson">
+        <ns6:ContactPerson>
+            <ns5:Name>TON s.r.o.</ns5:Name>
+        </ns6:ContactPerson>
+        </ns4:updateContactPerson>
+        </soap:Body></soap:Envelope>';
+        
+        
+        $resp = $this->tclass->handle($req);
+        
+        print_r($resp);
+    }
+}
